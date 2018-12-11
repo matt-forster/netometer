53f255f203f341ca917af01f0ce6c3e92d5d7687
Tyler Stewart <tylers490@gmail.com>
2018-12-11 13:05:25 -0700

    fix(fast): get additional information

--- src/providers/fast.test.js
+++ src/providers/fast.test.js
@@ -1,13 +1,21 @@
 const fast = require('./fast');
 
-describe('#providers // fast', function () {
+describe.only('#providers // fast', function () {
   it('test', async function () {
     jest.setTimeout(70000);
 
     const result = await fast.test();
 
     expect(result).toHaveProperty('down');
-    expect(result.down).toHaveProperty('speed');
+    expect(result.down).toHaveProperty('value');
     expect(result.down).toHaveProperty('unit');
+
+    expect(result).toHaveProperty('up');
+    expect(result.up).toHaveProperty('value');
+    expect(result.up).toHaveProperty('unit');
+
+    expect(result).toHaveProperty('ping');
+    expect(result.ping).toHaveProperty('value');
+    expect(result.ping).toHaveProperty('unit');
   });
 });
